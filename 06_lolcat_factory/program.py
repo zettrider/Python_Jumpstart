0b0f94e6404b09666ec3cc2d3d9299ec5b194a1a
zettrider <nico@schemberg.net>
2018-08-04 14:38:27 +0200

    finished 06 - started 07

---
+++ 06_lolcat_factory/program.py
@@ -0,0 +1,60 @@
+import platform
+import subprocess
+import os
+
+import cat_service
+
+
+def main():
+    print_header()
+    folder = get_or_create_output_folder()
+    print('Found or created folder: ' + folder)
+    download_cats(folder)
+    display_cats(folder)
+
+
+def print_header():
+    print('---------------------------------------')
+    print('          CAT FACTORY')
+    print('---------------------------------------')
+
+
+def get_or_create_output_folder():
+    base_folder = os.path.dirname(__file__)
+    folder = 'cat_pictures'
+    full_path = os.path.join(base_folder, folder)
+
+    if not os.path.exists(full_path) or not os.path.isdir(full_path):
+        print('reating new directory at {}'.format(full_path))
+        os.mkdir(full_path)
+
+    return full_path
+
+
+def download_cats(folder):
+    print('Contacting server to download cats...')
+    cat_count = 8
+    for i in range(1, cat_count + 1):
+        name = 'lolcat {}'.format(i)
+        print('Downloading cat ' + name)
+        cat_service.get_cat(folder, name)
+
+    print('done')
+
+
+def display_cats(folder):
+    # open folder
+    print('Displaying cats in OS Windows')
+    if platform.system() == 'Darwin':
+        subprocess.call(['open', folder])
+    elif platform.system() == 'Windows':
+        print(folder)
+        subprocess.call(['start', folder], shell=True)
+        # subprocess.call(['explorer', folder])
+    elif platform.system() == 'Windows':
+        subprocess.call(['xdg-open', folder])
+    else:
+        print("We don't support your os: " + platform.system())
+
+if __name__ == '__main__':
+    main()
---
+++ 07_wizard_battle/program.py
@@ -0,0 +1,63 @@
+# import actors
+import random
+import time
+
+from actors import Wizard, Creature, SmallAnimal
+
+
+def main():
+    print_header()
+    game_loop()
+
+
+def print_header():
+    print('---------------------------------------')
+    print('          WIZARD GAME APP')
+    print('---------------------------------------')
+
+
+def game_loop():
+    creatures = [
+        SmallAnimal('Toad', 1),
+        Creature('Tiger', 12),
+        SmallAnimal('Bat', 3),
+        #Dragon('Dragon', 50, scaliness=20),
+        Wizard('Evil Wizard', 1000),
+    ]
+
+    # print(creatures)
+
+    hero = Wizard('Gandolf', 75)
+
+    while True:
+
+        active_creature = random.choice(creatures)
+        print('A {} of level {} has appeared from a dark and foggy forest...'
+              .format(active_creature.name, active_creature.level))
+
+        cmd = input('Do you [a]ttack, [r]unaway, or [l]ook around? ')
+        if cmd == 'a':
+            if hero.attack(active_creature):
+                creatures.remove(active_creature)
+            else:
+                print("The wizard runs and hides taking time to recover...")
+                time.sleep(5)
+                print("The wizard returns revitalized!")
+        elif cmd == 'r':
+            print('The wizard has become unsure of his power and flees!!!')
+        elif cmd == 'l':
+            print('The wizard {} takes in the surroundings and sees:'.format(hero.name))
+            for c in creatures:
+                print (' * {} of level {}'.format(c.name, c.level))
+        else:
+            print("OK, exiting game... bye!")
+            break
+
+        if not creatures:
+            print("You've defeated all the creatures, well done!")
+            break
+
+
+
+if __name__ == '__main__':
+    main()
